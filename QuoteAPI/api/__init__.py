12d24b5f9555895f69f81f55fc689cdd760f7d5e
Murat Bayramukov <bay733@mail.ru>
2024-10-06 15:18:10 +0300

    lesson

--- QuoteAPI/api/__init__.py
+++ QuoteAPI/api/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+
 
 class Base(DeclarativeBase):
     pass
-
 
 
 app = Flask(__name__)
@@ -14,7 +15,12 @@ app.config.from_object("config.Config")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 migrate = Migrate(app, db)
+# Solution
+# https://stackoverflow.com/questions/59455520/flask-sqlalchemy-marshmallow-error-on-relationship-one-to-many
+ma = Marshmallow()
+ma.init_app(app)
 
-# TODO. Обязательно добавить импорт для обработчиков author и quote
+
+# DONE. Обязательно добавить импорт для обработчиков author и quote
 from api.handlers import author
 from api.handlers import quote
